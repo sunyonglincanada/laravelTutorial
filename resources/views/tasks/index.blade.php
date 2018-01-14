998a4cb4977131963592801b28f3fa194ffa3ba2
Yonglin Sun <sunyonglincanada@gmail.com>
2018-01-14 09:24:04 -0500

    working with query builder, routes and blades.

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+
+</head>
+<body>
+    <ul>
+        @foreach ($tasks as $task)
+            <li>
+                <a href="/tasks/{{$task->id}}">
+                    {{ $task->body }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Tasks
+Route::get('/tasks', function() {
+
+    $tasks = DB::table('tasks')->latest()->get();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function($id) {
+
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
+});
