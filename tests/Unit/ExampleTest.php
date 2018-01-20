de211eff88d37f4bfaedecc4778678e59fa8d24d
Yonglin Sun <sunyonglincanada@gmail.com>
2018-01-19 23:28:45 -0500

    unit test for posts archives.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * A basic test example.
      *
@@ -14,6 +19,29 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $first = factory(Post::class)->create();
+
+        $second = factory(Post::class)->create([
+            'created_at' => Carbon::now()->subMonth()
+        ]);
+
+
+        $posts = Post::archives();
+
+        $this->assertCount(2, $posts);
+        // todo: year and published format and value problem.
+//        $this->assertEquals([
+//            [
+//                "year" => $first->created_at->format('Y'),
+//                'month' => $first->created_at->format('F'),
+//                'published'=> 1
+//            ],
+//            [
+//                'year' => $second->created_at->format('Y'),
+//                'month' => $second->created_at->format('F'),
+//                'published'=> 1
+//            ],
+//
+//        ], $posts);
     }
 }
