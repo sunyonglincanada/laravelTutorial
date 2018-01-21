7fabcc572d55b4be6c63411defd2c3f02ab80148
Yonglin Sun <sunyonglincanada@gmail.com>
2018-01-20 23:14:25 -0500

    send markdown emails to new register uesr.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -62,10 +63,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        // Send an email to new registration user.
+        Mail::to($user)->send(new \App\Mail\RegisterUser($user));
+
+        return $user;
     }
 }
